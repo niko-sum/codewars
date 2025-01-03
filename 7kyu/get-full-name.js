14106821955da7c117f692cf855d6e90c02473bb
Niko Sumcad <nikolaisumcad@gmail.com>
2025-01-02 19:00:15 -0800

    added 7kyu solution

---
+++ 7kyu/get-full-name.js
@@ -0,0 +1,31 @@
+// The code provided is supposed return a person's Full Name given their first and last names.
+
+// But it's not working properly.
+
+// Notes
+// The first and/or last names are never null, but may be empty.
+
+// Task
+// Fix the bug so we can all go home early.
+
+// My solution
+
+class Dinglemouse{
+
+    constructor( firstName, lastName ){
+      this.first = firstName,
+      this.last = lastName
+    }
+    
+    getFullName(){
+      if (!this.first){
+        return this.last
+      } else if (!this.last){
+        return this.first
+      } else {
+        return this.first + " " + this.last
+      }
+        
+    }
+    
+  }
